4b20c34e960e19d3235bb8729427f1f1e29c135e
bogdanovso <inostc@bk.ru>
2020-07-10 11:39:37 +0300

    '12'

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -4,6 +4,29 @@ import {createTable} from '@/components/table/table.template'
 export class Table extends ExcelComponent {
 	static className = 'excel__table'
 
+	constructor($root) {
+		super($root, {
+			name: 'Table',
+			listeners: ['click', 'mousedown', 'mousemove', 'mouseup']
+		})
+	}
+
+	onClick() {
+		console.log('click')
+	}
+
+	onMousedown() {
+		console.log('mousedown')
+	}
+
+	onMousemove() {
+		console.log('mousemuve')
+	}
+
+	onMouseup() {
+		console.log('mouseup')
+	}
+
 	toHTML() {
 	  return createTable(20)
 	}
